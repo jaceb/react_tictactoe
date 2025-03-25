4ba3f70793b76919b16d0d30b8580e2841a77d2c
Jace <jacebriggswebdev@gmail.com>
2025-03-25 17:09:08 -0600

    finished game. game over shows and rematch button resets game. inputted username shows on gameover screen

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,19 @@
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-    ];
 
-export default function GameBoard({ onSelectSquare, turns }) {
+export default function GameBoard({ onSelectSquare, board }) {
 
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-        gameBoard[row][col] = player;
-    }
-/*
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            console.log('x');
-            return updatedBoard;
-        });
-        onSelectSquare();
-    }
-*/
-
+ 
     return <ol id="game-board">
-               {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+               {board.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
-                       {row.map((playerSymbol, colIndex) => <li key={colIndex}> <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+                       {row.map((playerSymbol, colIndex) => (
+                           <li key={colIndex}>
+                               <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                   disabled={playerSymbol !== null}>
+                                   {playerSymbol}
+                               </button>
+                           </li>
+                       ))}
             </ol>
         </li>)
         }
